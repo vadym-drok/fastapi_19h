8d9555d1e35aa9cb8aec7b1cea16f511a3a341a7
v.drok <v.drok@hicell.tel>
2024-06-15 12:52:14 +0300

    response_model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from .models import Post
-
-
+from .schemas import PostCreate, PostResponse
 from . import models
 from .database import engine, get_db
 
@@ -12,36 +13,30 @@ app = FastAPI(docs_url='/')
 # /redoc
 
 
-from pydantic import BaseModel
-class PostForm(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
 @app.get('/posts')
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), response_model=List[PostResponse]):
     posts = db.query(Post).all()
 
-    return {'data': posts}
+    return posts
 
 @app.get('/posts/{id}')
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), response_model=PostResponse):
     post = db.query(Post).filter(Post.id == id).first()
 
     if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-    return {'data': post}
+    return post
 
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-def create_post(post: PostForm, db: Session = Depends(get_db)):
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+def create_post(post: PostCreate, db: Session = Depends(get_db)):
     new_post = Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {'data': new_post}
+    return new_post
 
 
 @app.delete('/posts/{id}')
@@ -54,11 +49,11 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.delete(post)
     db.commit()
 
-    return {'data': post}
+    return post
 
 
 @app.put('/posts/{id}')
-def update_post(id: int, post: PostForm, db: Session = Depends(get_db)):
+def update_post(id: int, post: PostCreate, db: Session = Depends(get_db), response_model=PostResponse):
     post_query = db.query(Post).filter(Post.id == id)
     updated_post = post_query.first()
 
@@ -69,4 +64,4 @@ def update_post(id: int, post: PostForm, db: Session = Depends(get_db)):
 
     db.commit()
 
-    return {'data': post_query.first()}
+    return post_query.first()
