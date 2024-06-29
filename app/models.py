11d4130607898b11a885cef7ff874e4bd816723b
v.drok <v.drok@hicell.tel>
2024-06-29 10:37:20 +0300

    relationship for owner

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
@@ -14,6 +16,7 @@ class Post(Base):
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
     owner_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    owner = relationship("User")
 
 
 class User(Base):
--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,25 @@ from typing import Optional, Union
 from pydantic import BaseModel, EmailStr
 
 
+# User
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+    name: str
+
+
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# Post
 class PostBase(BaseModel):
     title: str
     content: str
@@ -18,24 +37,7 @@ class PostResponse(PostBase):
     id: int
     created_at: datetime
     owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-# User
-class UserBase(BaseModel):
-    email: EmailStr
-
-
-class UserCreate(UserBase):
-    password: str
-    name: str
-
-
-class UserResponse(UserBase):
-    id: int
-    created_at: datetime
+    owner: UserResponse
 
     class Config:
         orm_mode = True
