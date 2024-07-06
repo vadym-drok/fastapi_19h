c224be8e5efc20c6a1ff7c9a058c7471de4b69e6
v.drok <v.drok@hicell.tel>
2024-07-06 16:20:38 +0300

    alembic add to docker, add migrations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from . import models
 # from app.database import engine
 from app.routers import posts, users, auth, votes
@@ -8,6 +9,21 @@ from app.routers import posts, users, auth, votes
 
 app = FastAPI(docs_url='/')
 # /redoc
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(posts.router)
